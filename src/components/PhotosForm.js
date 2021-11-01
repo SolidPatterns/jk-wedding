aa269993458b1b44e8670dee5eb59d31bba69f62
Kemal Gültekin <SolidPatterns@users.noreply.github.com>
2021-11-01 23:38:51 +0100

    * moved the hide class to base scss
    * hid the upload photos link for now

--- src/components/PhotosForm.js
+++ src/components/PhotosForm.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import thankYouCaillou from '../images/thank-you.png'
 
 class PhotosForm extends React.Component {
--- src/pages/photos.js
+++ src/pages/photos.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import PhotosForm from '../components/PhotosForm'
 
 const SecondPage = () => (
   <Layout>
     <h1>Upload Photos</h1>
-    <p>We know that you are ... upload ...</p>
+
+    <PhotosForm />
 
     <Link to="/">Go back to the homepage</Link>
   </Layout>
